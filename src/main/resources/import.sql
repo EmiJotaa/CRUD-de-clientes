16a4c01f789dd54b06e7c9dfeddc445173195da1
Marco Júnior <mrcez4rr@gmail.com>
2025-02-03 09:55:51 -0300

    Seeding

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,1 +1,14 @@
-INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Marco Júnior', '053.248.641-29', 800.0, '1998-08-22', 0);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('José Jeferson', '053.248.641-27', 800.0, '1998-08-22', 2);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Marco Oliveira', '055.255.244-27', 1800.0, '1998-07-22', 4);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Lucas Santana', '055.255.254-27', 900.0, '1998-08-22', 1);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Luara Lourenço', '248.295.565-27', 2000.0, '1996-08-22', 5);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Kelvyn Chavier', '093.248.641-19', 1000.0, '1998-08-22', 0);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Chico Moedas', '083.248.641-39', 2800.0, '1998-08-23', 1);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Nélio Superior', '009.222.999-99', 22000.0, '1998-08-22', 2);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Marilha Mendes', '023.248.641-59', 4800.0, '1998-08-12', 4);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Ana Vitória', '248.238.641-99', 500.0, '1998-08-22', 7);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Gabriel Scarpanti', '235.278.641-39', 700.0, '1998-05-22', 8);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Gomes Rangel', '153.248.641-29', 1500.0, '1998-08-21', 0);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Laís Oliveira', '053.218.462-79', 1400.0, '1998-08-06', 1);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Robson Leblanc', '563.248.623-29', 1200.0, '1998-08-03', 2);
+
