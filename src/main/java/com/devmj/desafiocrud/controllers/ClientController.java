31229d9ecf145916441060c2c1625b93709bfe2e
Marco Júnior <mrcez4rr@gmail.com>
2025-02-03 10:41:42 -0300

    POST clients

--- src/main/java/com/devmj/desafiocrud/controllers/ClientController.java
+++ src/main/java/com/devmj/desafiocrud/controllers/ClientController.java
@@ -1,12 +1,18 @@
 package com.devmj.desafiocrud.controllers;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.devmj.desafiocrud.dto.ClientDTO;
 import com.devmj.desafiocrud.services.ClientService;
@@ -18,13 +24,21 @@ public class ClientController {
 	@Autowired
 	private ClientService service;
 	@GetMapping(value = "/{id}")
-	public ClientDTO findById(@PathVariable Long id) {
+	public ResponseEntity<ClientDTO> findById(@PathVariable Long id) {
 		ClientDTO dto = service.findById(id);
-		return dto;
+		return ResponseEntity.ok(dto);
 	}
 	
 	@GetMapping
 	public Page<ClientDTO> findAll(Pageable pageable) {
 		return service.findAll(pageable);
+	}
+	
+	@PostMapping
+	public ResponseEntity<ClientDTO> insert(@RequestBody ClientDTO dto) {
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
 	}
 }
